0a0ebd34854c203305ece18660a60926a7feaf81
musbahteste <contadomusbah@gmail.com>
2025-11-16 13:50:31 -0300

    1.0.5

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // !! ATENÇÃO !!
+    // Permite perigosamente que builds de produção sejam concluídos
+    // mesmo que seu projeto tenha erros de tipo (TypeScript).
+    // Use isso como uma solução temporária para o bug de tipos do Next.js 16.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
